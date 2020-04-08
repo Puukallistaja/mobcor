3f02154f31096ca61541420aa2dc56ecb77b5fa1
Gabriel Barszczowski <gabriel.barszczowski@unity.pl>
2020-04-08 13:55:29 +0200

    add dto

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Request, Get, Post, Param, UseGuards } from '@nestjs/common';
+import { Controller, Body, Get, Post, Param, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiParam, ApiBody } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { LoginUserDto } from "./auth/dto/login-user.dto";
 
 @Controller()
 export class AppController {
@@ -11,7 +12,7 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-  
+
   @Get('user/:id')
   @ApiParam({name: 'id', type: Number})
   getUser(@Param() params): string {
@@ -20,7 +21,7 @@ export class AppController {
 
   @UseGuards(AuthGuard('local'))
   @Post('auth/login')
-  async login(@Request() req) {
-    return req.user;
+  async login(@Body() user: LoginUserDto) {
+    return user;
   }
 }
